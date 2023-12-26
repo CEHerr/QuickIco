8851b985a7e665df87b6653c2edec01a045c00af
Charles Herr <cha26243@gmail.com>
2023-12-26 15:15:32 -0500

    Removed Depreicated Files

--- QuickIco/FolderOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿public static class FolderOptionsFactory {
-    static public FolderOptions CreateOptions() {
-        return new FolderOptions();
-    }
-}
-
-public class FolderOptions {
-    public FolderOptions() { }
-}
